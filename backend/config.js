63a26121583fb549c2ea808275e8b580dc3decc6
Vboxmoh <mohamedmondher61@gmail.com>
2026-06-04 06:53:44 +0200

    Add Dockerfiles and update config files for production

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    CORS_ORIGIN: 'http://localhost:3000'
+    CORS_ORIGIN: 'http://devops-tc1-alb-679051767.eu-west-3.elb.amazonaws.com'
 }
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8080/'
+export const API_URL = 'http://devops-tc1-alb-679051767.eu-west-3.elb.amazonaws.com/api'
 export default API_URL
